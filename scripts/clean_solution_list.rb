1112606746613fdbd6a53f6ef56b28fffebae2fa
Jan Milosh <janmilosh@gmail.com>
2022-02-05 08:43:43 -0500

    Clean up 5 letter solution list

--- scripts/clean_solution_list.rb
+++ scripts/clean_solution_list.rb
@@ -9,16 +9,19 @@
 # File.open('app/wordlists/4_reference.txt', 'w') { |f| f.write(serialized_array) }
 
 # four_ref = Marshal.load File.read('app/wordlists/4_reference.txt')
-five_old_sol = Marshal.load File.read('app/wordlists/5_solution.txt')
-five_new_sol = five_old_sol
+five_old_sol = Marshal.load File.read('app/wordlists/5_solution_original.txt')
+five_new_sol = five_old_sol.map(&:itself)
 
 five_old_sol.each do |w|
-  puts "delete #{w}?(y):"
+  puts "(y) to delete #{w}"
   delete = gets.chomp
   if delete == 'y'
     five_new_sol.delete(w)
   end
 end
+
+puts five_old_sol.size
+puts five_new_sol.size
 
 serialized_array = Marshal.dump(five_new_sol)
 File.open('app/wordlists/tmp', 'w') { |f| f.write(serialized_array) }
